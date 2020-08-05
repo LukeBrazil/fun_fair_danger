9c6de6c6c265289193d1f17a38619d28b54ced2f
Luke Brazil <Luke.brazil13@icloud.com>
2020-08-05 11:03:20 -0500

    Initial commit for axe room.

---
+++ axe_room.py
@@ -0,0 +1,57 @@
+import random
+
+
+
+# def axethrow():
+#     print('Three targets appear!')
+#     target = int(input('Throw your axe at Target 1? Target 2? or Target 3?'))
+#     if target == 1:
+#         print('You missed!')
+#         axe_total - 1
+#     elif target == 2:
+#         print('Target hit!')
+#         axe_total - 1
+#         target_hit + 1
+#     elif target == 3:
+#         axe_total - 1
+#         print('Wow you suck!')
+        
+
+
+
+
+def main():
+    welcome = input('Welcome to the Axe throwing game! Do you have what it takes? Say yes or no...  ')
+    axe_total = 4
+    target_hit = 0
+    game_won = False
+    if game_won == True:
+        print('It is time to enter the third room congratulations!!! Do know what dangers lie ahead?')
+    if welcome.lower() == 'yes':
+        game_on = True
+    elif welcome.lower() == 'no':
+        main()
+    while game_on == True:
+        print('Three targets appear!')
+        target = int(input('Throw your axe at Target 1? Target 2? or Target 3?'))
+        print("Total Axes Remainig!: ")
+        print(axe_total)
+        print('Total Targets Hit!: ')
+        print(target_hit)
+        if target == 1:
+            print('You missed!')
+            axe_total -= 1
+        elif target == 2:
+            print('Target hit!')
+            axe_total -= 1
+            target_hit += 1
+        elif target == 3:
+            axe_total -= 1
+            print('Wow you suck!')
+    if target_hit == 3:
+        print('You won the game!')
+        
+
+
+
+main()
