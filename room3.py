708b44ea9be997bc97e1bce022b4a38a01261f82
Annemarie Thomas <athomas9sa@gmail.com>
2020-08-06 12:46:16 -0400

    lunchtime update

--- room3.py
+++ room3.py
@@ -1,11 +1,12 @@
 # ROOM3 ROTTEN TOMATOES: Go ahead , pick your poison, a bucket full of rotten tomatoes at your disposal.
 import random
-def main():
+def TheTomatoCave():
 
     rotten_tomatoes = 3 # get 3 chances for a hit (accumalator)
     game_running = True
     play_again = "y"
     troll_conquered = False
+    #room_3__key = goldentomato
 
     sarcasm_list =[
         "Well, aren't we just a ray of frigging sunshine.",
@@ -17,7 +18,7 @@ def main():
         "Aw, did I step on your poor little bitty ego?"
     ]
     print("""
-        Welcome to Room 3. 
+        Welcome to Room 3: Angry Elf and His TomatoCave.
 
         Yes, I know, a troll blocking the doorway.
 
@@ -25,7 +26,7 @@ def main():
 
         Try him, sneeze, laugh, have a pretend heart attacked!
 
-        Only advise, don't touch him, he can be a bit cranky!
+        Only advise, don't touch him, as here he is know as Cranky!
 
         Well, what are you waiting for, go on in!
 
@@ -35,9 +36,10 @@ def main():
     if question_one == "y": 
         print("OK, you asked for it!")
     elif question_one == "n": 
-        print("Troll approaching you")
+
+        print("Troll approaching you! No limits set!")
         print("""
-            Sorry!
+            Sorry! Only a small challenge!
         
             Bummer!
 
@@ -57,34 +59,34 @@ def main():
         hit = random.randint(0,100)
     
         if rotten_tomatoes == 3:
-            input("Hit enter, go for it, a rotten tomato strike!")
+            input("Welcome to Elf TomatoCave! Hit enter, go for it, a rotten tomato strike!")
             if hit == 2:
-                print("bingo!")
-                troll_conquered = True
-                break #win the game
+                #print("BINGO!")
+                elf_conquered = True
+                break #game over
             else:
                 print(sarcasm_list[value])
-                play_again = input("Would you like another chance?")
+                play_again = input("Would you like another chance? yes (y) or no (n)")
    
         if rotten_tomatoes == 2:
-            input("come on! any strickes this time?")
+            input("Get on with it! Hit enter! Any strickes this time?")
             if hit == 1:
-                print("ditto!")
-                troll_conquered = True
-                break #win the game
+                #print("DITTO!")
+                elf_conquered = True
+                break #game over
             else:
                 print(sarcasm_list[value])
                 play_again = input("Would you like another chance? yes (y) or no (n)")
 
         if rotten_tomatoes == 1:
-            input("go on, and hit enter!")
+            input("GO ON..., and hit enter again!")
             if hit == 0:
-                print("dammit!")
-                troll_conquered = True
-                break #win the game
+                #print("DAMMIT, was the last one?")
+                elf_conquered = True
+                break #game over
             else:
                 print(sarcasm_list[value])
-                print("You ran out of tomatoes!")
+                print("You're all out of tomatoes!")
         
         rotten_tomatoes -= 1 # game change here
        
@@ -94,9 +96,15 @@ def main():
                 rotten_tomatoes = 3
 
 
+    #print ("remember your key[room_3__key]")
+    print("""
+        With a healthy smile, muscles, bone and liver,
+        You're - Off to the next maize-ing thither,
+        Hopefully, not craving any liquor!
+        Escaped the Dragons and muscle Engraving!
+        Only to remember the golden tomato key-ring-er!
+        """)
 
-    print("Off to the next maize")
-
-main()
+TheTomatoCave()
 
 
