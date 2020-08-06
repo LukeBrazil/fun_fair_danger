ec500fea4d0b8ac11ddf970458a575101096bd03
Luke Brazil <Luke.brazil13@icloud.com>
2020-08-06 11:44:48 -0500

    Added random int function to determine if axe hit its mark.

--- axe_room.py
+++ axe_room.py
@@ -1,28 +1,19 @@
-
+import random 
 
 def axe_room():
-    print()
-    print('*******************************')
-    print()
-    print('Welcome to the Axe throwing game! Do you have what it takes?')
-    print()
-    print()
-    welcome = input('Please Enter the Key from Room 1: ')
-    print()
-    print()
-    axe_total = 7
+    print('Welcome to the Axe Room Challenge. Do you have what it takes?')
+    welcome = input('Please enter key from room 1: ')
+    axe_total = 10
     target_hit = 0
-    if welcome.lower() == 'key':
-        game_on = True
-    elif welcome.lower() != 'key':
-        axe_room()
-        game_on == True
-    while game_on == True and target_hit < 5:
+    game_on = True
+
+    while game_on == True and target_hit < 3:
         print('Three targets appear!')
         print()
-        target = input('Throw axe at target? Enter yes or no. If you enter anything differntely the program will probably break. So dont. :D   ')
+        target = random.randint(1, 6)
+        message = input('Throw your axe at the target?')
 
-        if target.lower() == 'yes' or 'no':
+        if target < 5 and message == 'yes':
             target_hit += 1
             axe_total -= 1
             print()
@@ -42,62 +33,31 @@ def axe_room():
             print()
             print()
         
-            if target_hit == 3:
-                print('*****************************************************')
-                print()
-                print('YOU WON THE GAME!!!!')
-                print()
-                print('Here is your Golden Axe! Use this Golden Axe to continue through the maze!')
-                print()
-                print('Your code for Room 2 is: GoldenAxe')
-                print()
-                print('*****************************************************')
-                break
-                
-
-axe_room()
-
-# import random
-
-
-# def axethrow():
-#     print('Three targets appear!')
-#     target = int(input('Throw your axe at Target 1? Target 2? or Target 3?'))
-#     if target == 1:
-#         print('You missed!')
-#         axe_total - 1
-#     elif target == 2:
-#         print('Target hit!')
-#         axe_total - 1
-#         target_hit + 1
-#     elif target == 3:
-#         axe_total - 1
-#         print('Wow you suck!')
-
-# def end_game():
-#     print("Congrats you have completed this challenge! Now time for a riddle...")
-#     print()
-        
-
-
-        # if target == 1:
-        #     print('You missed!')
-        #     axe_total -= 1
-        #     print("Total Axes Remaining!: ")
-        #     print(axe_total)
-        #     print('Total Targets Hit!: ')
-        #     print(target_hit)
-        # elif target == 2:
-        #     print('Target hit!')
-        #     axe_total -= 1
-        #     target_hit += 1
-        #     print(axe_total)
-        #     print('Total Targets Hit!: ')
-        #     print(target_hit)
-        # elif target == 3:
-        #     axe_total -= 1
-        #     print('Wow you suck!')    
-        #     print("Total Axes Remaining!: ")
-        #     print(axe_total)
-        #     print('Total Targets Hit!: ')
-        #     print(target_hit)
+        elif target > 4:
+            axe_total -= 1
+            print()
+            print('You Missed the target!')
+            print()
+            print('***************************')
+            print()
+            print('Total targets hit ')
+            print()
+            print(target_hit)
+            print()
+            print('Axes remaining')
+            print()
+            print(axe_total)
+            print()
+            print('***************************')
+            print()
+            print()
+    else:
+        print('*****************************************************')
+        print()
+        print('YOU WON THE GAME!!!!')
+        print()
+        print('Here is your Golden Axe! Use this Golden Axe to continue through the maze!')
+        print()
+        print('Your code for Room 2 is: GoldenAxe')
+        print()
+        print('*****************************************************')
